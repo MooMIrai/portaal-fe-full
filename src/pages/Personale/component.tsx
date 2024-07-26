347712994ceb96cd79c48d0860f4d68c0ca1205f
CalogeroMiraglia95q <c.miraglia@xister.com>
2024-07-26 13:15:37 +0200

    fixed reading and sync form personale

--- src/pages/Personale/component.tsx
+++ src/pages/Personale/component.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Tab from "common/Tab";
 import { TabStripSelectEventArguments } from '@progress/kendo-react-layout';
 import Form from "common/Form";
@@ -12,48 +12,68 @@ import { AnagraficaData, TrattamentoEconomicoData, RuoliData } from './modelForm
 
 const PersonaleSection: React.FC = () => {
     const [selected, setSelected] = useState(0);
-    const [formAnagraficaData, setFormAnagraficaData] = useState<AnagraficaData>({});
-    const [formTrattamentoEconomicoData, setFormTrattamentoEconomicoData] = useState<TrattamentoEconomicoData>({});
-    const [formRuoliData, setFormRuoliData] = useState<RuoliData>({});
+   
+    //const [formTrattamentoEconomicoData, setFormTrattamentoEconomicoData] = useState<TrattamentoEconomicoData>({});
+    //const [formTrattamentoEconomicoData, setFormTrattamentoEconomicoData] = useState<TrattamentoEconomicoData>({});
+    //const [formRuoliData, setFormRuoliData] = useState<RuoliData>({});
+
+    const formAnagrafica = useRef<any>();
+    const formTrattamentoEconomico = useRef<any>();
+    const formRuoli = useRef<any>();
 
     const handleSelect = (e: TabStripSelectEventArguments) => {
         setSelected(e.selected);
     };
 
-    const handleFieldChange = (fieldName: string, value: any) => {
-        switch (selected) {
-            case 0:
-                setFormAnagraficaData((prevData) => ({ ...prevData, [fieldName]: value }));
-                break;
-            case 1:
-                setFormTrattamentoEconomicoData((prevData) => ({ ...prevData, [fieldName]: value }));
-                break;
-            case 2:
-                setFormRuoliData((prevData) => ({ ...prevData, [fieldName]: value }));
-                break;
-            default:
-                break;
-        }
-    };
-
-
     const handleSubmit = () => {
+
+        let hasError=false
+
+        let formAnagraficaData:AnagraficaData={};
+        formAnagrafica.current?.onSubmit();
+        if(formAnagrafica.current?.isValid()){
+            formAnagraficaData=formAnagrafica.current.values;
+        }else{
+            hasError=true
+        }
+
+        let formTrattamentoEconomicoData:TrattamentoEconomicoData={};
+        formTrattamentoEconomico.current?.onSubmit();
+        if(formTrattamentoEconomico.current?.isValid()){
+            formTrattamentoEconomicoData=formTrattamentoEconomico.current.values;
+        }else{
+            hasError=true
+        }
+
+        let formRuoliData:RuoliData={};
+        formRuoli.current?.onSubmit();
+        if(formRuoli.current?.isValid()){
+            formRuoliData=formRuoli.current.values;
+        }else{
+            hasError=true;
+        }
+        
         const combinedData = {
             anagrafica: formAnagraficaData,
             trattamentoEconomico: formTrattamentoEconomicoData,
             ruoli: formRuoliData
         };
+
         console.log('Combined Data:', combinedData);
+
+        if(!hasError){
+            //TODO chiamata al BE
+        }
     };
 
     const getFormFields = () => {
         switch (selected) {
             case 0:
-                return { fields: getFormAnagraficaFields(formAnagraficaData), formData: formAnagraficaData };
+                return { fields: getFormAnagraficaFields({}), formData: {} };
             case 1:
-                return { fields: getFormTrattamentoEconomicoFields(formTrattamentoEconomicoData), formData: formTrattamentoEconomicoData };
+                return { fields: getFormTrattamentoEconomicoFields({}), formData: {} };
             case 2:
-                return { fields: getFormRuoliFields(formRuoliData), formData: formRuoliData };
+                return { fields: getFormRuoliFields({}), formData: {} };
             default:
                 return { fields: {}, formData: {} };
         }
@@ -68,36 +88,33 @@ const PersonaleSection: React.FC = () => {
                     {
                         title: "Anagrafica", children: <div className={styles.parentForm}>
                             <Form
-
+                                ref={formAnagrafica}
                                 fields={Object.values(fields)}
                                 formData={formData}
-                            /*     showSubmit={true}
-                                submitText={"Salva"} */
-                               /*  onSubmit={handleSubmit} */
+                                onSubmit={(data:AnagraficaData)=>data }
+                                description="Ana"
                             />
                         </div>
                     },
                     {
                         title: "Trattamento Economico", children: <div className={styles.parentForm}>
                             <Form
-
                                 fields={Object.values(fields)}
                                 formData={formData}
-                                /* showSubmit={true}
-                                submitText={"Salva"} */
-                               /*  onSubmit={handleSubmit} */
+                                ref={formTrattamentoEconomico}
+                                onSubmit={(data:TrattamentoEconomicoData)=>data }
+                                description="TE"
                             />
                         </div>
                     },
                     {
                         title: "Assegna Profilo", children: <div className={styles.parentForm}>
                             <Form
-
+                                ref={formRuoli}
                                 fields={Object.values(fields)}
                                 formData={formData}
-                              /*   showSubmit={true}
-                                submitText={"Salva"} */
-                              /*   onSubmit={handleSubmit} */
+                                onSubmit={(data:RuoliData)=>data }
+                                description="PR"
                             />
                         </div>
                     },
@@ -107,9 +124,8 @@ const PersonaleSection: React.FC = () => {
 
                                 fields={Object.values(fields)}
                                 formData={formData}
-                             /*    showSubmit={true}
-                                submitText={"Salva"} */
-                               /*  onSubmit={handleSubmit} */
+                                description="per"
+                             
                             />
                         </div>
                     }
