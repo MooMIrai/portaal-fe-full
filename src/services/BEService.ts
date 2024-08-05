87e5255e4b872a20ec6b4ca6ab05c2321b6d001a
ElisaGiu <elisagiurelli@taal.it>
2024-08-05 16:00:24 +0200

    commit be services

--- src/services/BEService.ts
+++ src/services/BEService.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import AuthService from './AuthService';
 
 const client = axios.create({
-    baseURL:'http://localhost:3001'
+    baseURL:'https://localhost:3001'
 });
 
 client.interceptors.request.use((config)=>{
