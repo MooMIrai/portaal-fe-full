fa3a66e2bc179170cc8dc6a335b5de0b528a700e
Francesco Ferrari <fferrari@taal.it>
2024-08-09 11:50:49 +0200

    fix permanent token

--- src/components/CalendarMobile/component.tsx
+++ src/components/CalendarMobile/component.tsx
@@ -2,24 +2,62 @@ import * as React from "react";
 import { Calendar, CalendarProps } from "@progress/kendo-react-dateinputs";
 import CustomCell from "./CustomCell/component";
 import styles from "./style.module.scss";
+import { useState } from "react";
 
-interface CalendarMobileProps extends CalendarProps {}
+interface CalendarMobileProps extends CalendarProps {
+  data: Array<Record<string, any>>;
+  content?: (
+    slot: Record<string, any> | undefined,
+    closeModalCallback: () => void
+  ) => { component: JSX.Element; title: string };
+}
 
 export default function CalendarMobile(props: CalendarMobileProps) {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  console.log(props.content);
+
+  const renderCell = (cellProp: any) => {
+    const { date } = cellProp;
+    const dayActivities = props.data.filter(
+      (activity) =>
+        new Date(activity.start).toDateString() === date.toDateString()
+    );
+
+    const handleClick = () => {
+      setSelectedDate(date);
+    };
+
+    return (
+      <CustomCell {...cellProp} onClick={handleClick}>
+        <div className={styles.calendarDay}>
+          {cellProp.children}
+          {dayActivities.length > 0 && <div className={styles.eventDot}></div>}
+        </div>
+      </CustomCell>
+    );
+  };
+
+  const closeModal = () => {
+    setSelectedDate(null);
+  };
+
+  const content =
+    selectedDate && props.content
+      ? props.content({ day: selectedDate.getDate() }, closeModal)
+      : null;
+
   return (
-    <Calendar
-      className={styles.container}
-      smoothScroll={true}
-      showOtherMonthDays={false}
-      mobileMode={false}
-      cell={(cellProp) => (
-        <CustomCell {...cellProp}>
-          <div className={styles.calendarDay}>
-            {cellProp.children} <div className={styles.eventDot}></div>
-          </div>
-        </CustomCell>
-      )}
-      {...props}
-    />
+    <div className={styles.calendarContainer}>
+      <Calendar
+        className={styles.container}
+        smoothScroll={false}
+        showOtherMonthDays={true}
+        mobileMode={false}
+        navigation={false}
+        cell={renderCell}
+        {...props}
+      />
+      <div>{props?.content?.({ day: 2 }, closeModal).component}</div>
+    </div>
   );
 }
