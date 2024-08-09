25b664c87cdf05b970e06ba9c9f8a76e540eddcc
Francesco Ferrari <fferrari@taal.it>
2024-08-09 11:52:07 +0200

    indent app

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,27 @@ import Drawer from "common/Drawer";
 import Theme from "common/Theme";
 import authService from "common/services/AuthService";
 
-import AuthRoutes from 'auth/Routes';
-import LookupsRoutes from 'lookups/Routes';
-import initLookupFn from 'lookups/Index';
-import HRRoutes from 'hr/Routes';
-import initHRFn from 'hr/Index';
+import AuthRoutes from "auth/Routes";
+import LookupsRoutes from "lookups/Routes";
+import initLookupFn from "lookups/Index";
+import HRRoutes from "hr/Routes";
+import initHRFn from "hr/Index";
 import { Routes, useNavigate } from "react-router-dom";
 
-
 export const App = () => {
-
   const [routes, setRoutes] = useState<Array<any>>([]);
-  const [token,setToken] = useState<string>();
+  const [token, setToken] = useState<string>();
   const navigate = useNavigate();
 
-  useEffect(()=>{
-    try{
-      setToken(authService.getToken())
-    }catch{
-      
-    }
-   
-  },[])
-
+  useEffect(() => {
+    try {
+      setToken(authService.getToken());
+    } catch {}
+  }, []);
 
   useEffect(() => {
     let r: Array<any> = [];
-    if(token){
-      
+    if (token) {
       const lookupConfig = initLookupFn();
       if (lookupConfig && lookupConfig.menuItems) {
         r = [...r, ...lookupConfig.menuItems];
@@ -39,35 +32,35 @@ export const App = () => {
       if (hrConfig && hrConfig.menuItems) {
         r = [...r, ...hrConfig.menuItems];
       }
-      window.addEventListener("LOGOUT",()=>{setToken(undefined)},{once:true});
-    }else{
-      navigate('/login',{replace:true});
+      window.addEventListener(
+        "LOGOUT",
+        () => {
+          setToken(undefined);
+        },
+        { once: true }
+      );
+    } else {
+      navigate("/login", { replace: true });
     }
-    
+
     setRoutes(r);
+  }, [token]);
 
-  }, [token])
+  if (!token && location.pathname != "/login") {
+    location.href = "/login";
+  }
+  if (!token) {
+    return <AuthRoutes />;
+  }
 
-   if(!token && location.pathname!='/login'){
-    
-    location.href='/login';
-  }
-  if(!token){
-    return <AuthRoutes />
-  }
- 
   return (
     <>
-      
-        <Theme>
-          <Drawer items={routes} >
-            <LookupsRoutes />
-            <HRRoutes />
-          </Drawer>
-        </Theme>
-      
+      <Theme>
+        <Drawer items={routes}>
+          <LookupsRoutes />
+          <HRRoutes />
+        </Drawer>
+      </Theme>
     </>
-
-
   );
 };
