1cf0131c248cb9f37a218fa21ad5fef45a51dd4d
Francesco <fferrari@taal.it>
2024-08-09 11:50:00 +0200

    fixed endpoint redirect

--- src/pages/LoginGoogle/component.tsx
+++ src/pages/LoginGoogle/component.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import { Button } from '@progress/kendo-react-buttons';
-import { Card } from '@progress/kendo-react-layout';
-import  styles from './styles.module.css'
-
+import React from "react";
+import { Button } from "@progress/kendo-react-buttons";
+import { Card } from "@progress/kendo-react-layout";
+import client from "common/services/BEService";
+import styles from "./styles.module.css";
 
 const LoginGoogleComponent = () => {
   const handleGoogleLogin = async () => {
     try {
-
-    window.location.href = 'http://localhost:3001/auth/';  
+      window.location.href = client.defaults.baseURL + "/auth/";
     } catch (error) {
       console.error("Error during Google login:", error);
     }
@@ -16,18 +15,25 @@ const LoginGoogleComponent = () => {
 
   return (
     <div className={styles.loginContainer}>
-       
-    <Card className={styles.loginCard}>
-      <div className={styles.loginContent}>
-      <img src="/image/logoTaal.png" alt="TAAL Logo" className={styles.logo} />
-        <h1>Benvenuto!</h1>
-        <p>Accedi al dominio TAAL</p>
-        <Button onClick={handleGoogleLogin} className={`${styles.kButton} k-button`} icon="google">
-          Accedi con Google
-        </Button>
-      </div>
-    </Card>
-  </div>
+      <Card className={styles.loginCard}>
+        <div className={styles.loginContent}>
+          <img
+            src="/image/logoTaal.png"
+            alt="TAAL Logo"
+            className={styles.logo}
+          />
+          <h1>Benvenuto!</h1>
+          <p>Accedi al dominio TAAL</p>
+          <Button
+            onClick={handleGoogleLogin}
+            className={`${styles.kButton} k-button`}
+            icon="google"
+          >
+            Accedi con Google
+          </Button>
+        </div>
+      </Card>
+    </div>
   );
 };
 
--- src/remote.d.ts
+++ src/remote.d.ts
@@ -1,4 +1,3 @@
-
-
 declare module "common/DrawerService";
-declare module "common/services/AuthService"
+declare module "common/services/AuthService";
+declare module "common/services/BEService";
