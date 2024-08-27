366dd12d06f03bf022bcc242435d4f069c53bf17
CalogeroMiraglia95q <c.miraglia@xister.com>
2024-08-27 15:37:30 +0200

    added environmental

--- webpack.config.js
+++ webpack.config.js
@@ -3,32 +3,49 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const Dotenv = require("dotenv-webpack");
 const deps = require("./package.json").dependencies;
 const { FederatedTypesPlugin } = require("@module-federation/typescript");
-const mfeConfig = {
-  name: "core",
-  filename: "core.js",
-  remotes: {
+const webpack = require("webpack");
+
+const mfeConfig = (path,mode)=>{
+  let remotes = {
     common: "common@http://localhost:3003/remoteEntry.js",
     auth: "auth@http://localhost:3006/remoteEntry.js",
     lookups: "lookups@http://localhost:3005/remoteEntry.js",
     hr: "hr@http://localhost:3008/remoteEntry.js",
-  },
-  shared: {
-    ...deps,
-    react: {
-      singleton: true,
-      requiredVersion: deps.react,
+  }
+  if(mode==='production'){
+    remotes = {
+      common: "common@"+path+"/common/remoteEntry.js",
+      auth: "auth@"+path +"/auth/remoteEntry.js",
+      lookups: "lookups@"+path +"/lookups/remoteEntry.js",
+      hr: "hr@"+path +"/hr/remoteEntry.js",
+    }
+  }
+  return {
+    name: "core",
+    filename: "core.js",
+    remotes: remotes,
+    shared: {
+      ...deps,
+      react: {
+        singleton: true,
+        requiredVersion: deps.react,
+      },
+      "react-dom": {
+        singleton: true,
+        requiredVersion: deps["react-dom"],
+      },
     },
-    "react-dom": {
-      singleton: true,
-      requiredVersion: deps["react-dom"],
-    },
-  },
+  }
 };
-module.exports = (_, argv) => ({
+module.exports = (_, argv) => {
+  require('dotenv').config({path:'./.env.'+argv.mode});
+
+  return {
   entry: "./src/index",
   mode: "development",
   output: {
-    publicPath: "http://localhost:3000/",
+    publicPath: process.env.RELEASE_PATH,
+    clean: true
   },
   optimization: {
     splitChunks: {
@@ -66,13 +83,17 @@ module.exports = (_, argv) => ({
     ],
   },
   plugins: [
-    new ModuleFederationPlugin(mfeConfig),
+    new ModuleFederationPlugin(mfeConfig(process.env.REMOTE_PATH,argv.mode)),
     //new FederatedTypesPlugin({ federationConfig: mfeConfig }),
     new HtmlWebPackPlugin({
       template: "./src/index.html",
     }),
-    new Dotenv(),
+    new Dotenv({path:'./.env.'+argv.mode}),
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1,
+    })
   ],
 
   devtool: "source-map",
-});
+}
+};
