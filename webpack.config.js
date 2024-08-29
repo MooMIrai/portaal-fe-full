b83714c035ce0ee1cc7af91835270c1b1254a5fb
CalogeroMiraglia95q <c.miraglia@xister.com>
2024-08-29 10:22:01 +0200

    fixing lookups local launch

--- webpack.config.js
+++ webpack.config.js
@@ -75,9 +75,6 @@ module.exports = (_, argv) => {
       template: "./src/index.html",
     }),
     new Dotenv({path:'./.env.'+argv.mode}),
-    new Dotenv({path:'./.env.'+argv.mode}),
-    ...(argv.mode==='production'?[new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1,
-    })]:[]),
+    
   ],
 }};
