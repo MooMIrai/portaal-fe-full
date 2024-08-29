044bb4f478c21f334bb46466ce5e135719a7139e
Francesco <fferrari@taal.it>
2024-08-29 11:06:52 +0200

    handle actions conditions

--- src/pages/Offerte/component.tsx
+++ src/pages/Offerte/component.tsx
@@ -4,124 +4,157 @@ import GridTable from "common/Table";
 import { offertaService } from "../../services/offertaService";
 import { fromOfferBEModelToOfferModel } from "../../adapters/offertaAdapters";
 import { OffertaCrud } from "../../component/OffertaCrud/component";
-import CountrySelector from 'common/CountrySelector';
+import CountrySelector from "common/CountrySelector";
 
 const columns = [
-    { key: "customer_name", label: "Cliente", type: "string", sortable: true, filter: "text" },
-    { key: "accountManager.name", label: "Commerciale", type: "string", sortable: true, filter: "text" },
-    { key: "protocol", label: "Protocollo", type: "string", sortable: true, filter: "text" },
-    { key: "title", label: "Titolo", type: "string", sortable: true, filter: "text" },
-    { key: "description", label: "Descrizione", type: "string", sortable: true, filter: "text" },
-    { key: "creation_date", label: "Data creazione", type: "date", sortable: true, filter: "date" },
-    
-  ];
-  
+  {
+    key: "customer_name",
+    label: "Cliente",
+    type: "string",
+    sortable: true,
+    filter: "text",
+  },
+  {
+    key: "accountManager.name",
+    label: "Commerciale",
+    type: "string",
+    sortable: true,
+    filter: "text",
+  },
+  {
+    key: "protocol",
+    label: "Protocollo",
+    type: "string",
+    sortable: true,
+    filter: "text",
+  },
+  {
+    key: "title",
+    label: "Titolo",
+    type: "string",
+    sortable: true,
+    filter: "text",
+  },
+  {
+    key: "description",
+    label: "Descrizione",
+    type: "string",
+    sortable: true,
+    filter: "text",
+  },
+  {
+    key: "creation_date",
+    label: "Data creazione",
+    type: "date",
+    sortable: true,
+    filter: "date",
+  },
+];
 
-export default function OffertePage(){
-    const [filter, setFilter] = useState();
-    const [sorting, setSorting] = useState([]);
-    const [pagination] = useState({ currentPage: 1, pageSize: 10 });
+export default function OffertePage() {
+  const [filter, setFilter] = useState();
+  const [sorting, setSorting] = useState([]);
+  const [pagination] = useState({ currentPage: 1, pageSize: 10 });
 
+  const loadData = async (
+    pagination: any,
+    filter: any,
+    sorting: any[],
+    term?: string
+  ) => {
+    const include = true;
 
-    const loadData = async (
-        pagination: any,
-        filter: any,
-        sorting: any[],
-        term?: string
-      ) => {
-        const include= true;
-       
+    const tableResponse = await offertaService.search(
+      pagination.currentPage,
+      pagination.pageSize,
+      filter,
+      sorting,
+      term,
+      include
+    );
 
-       const tableResponse= await offertaService.search(
-          pagination.currentPage,
-          pagination.pageSize,
-          filter,
-          sorting,
-          term,
-          include,
-        );
-        
+    return {
+      data: tableResponse.data.map(fromOfferBEModelToOfferModel),
+      meta: {
+        total: tableResponse.meta.model,
+      },
+    };
+  };
 
-        
+  useEffect(() => {
+    loadData(pagination, filter, sorting);
+  }, [pagination, filter, sorting]);
 
-        return{ 
-          data:tableResponse.data.map(fromOfferBEModelToOfferModel),
-          meta: {
-            total:tableResponse.meta.model
-          }
-        }
-        
-      };
+  const handleFormSubmit = (
+    type: string,
+    formData: any,
+    refreshTable: any,
+    id: any,
+    closeModal: () => void
+  ) => {
+    let promise: Promise<any> | undefined = undefined;
 
-    useEffect(() => {
-        loadData(pagination, filter, sorting)
-    }, [pagination, filter, sorting]);
-
-
-    const handleFormSubmit = (type: string, formData: any, refreshTable: any, id: any, closeModal:()=>void)=>{
-        let promise:Promise<any> | undefined=undefined;
-        
-        if (type === "create") {
-          promise = offertaService.createResource(formData);
-        } else if (type === "edit") {
-          promise =offertaService.updateResource(id,formData);
-        } else if (type === "delete") {
-          promise = offertaService.deleteResource(id);
-        }
-      
-
-        if(promise){
-          promise.then(()=>{
-            NotificationProviderActions.openModal({icon:true,style:'success'},"Operazione avvenuta con successo");
-            refreshTable();
-            closeModal();
-
-          })
-        }
-
+    if (type === "create") {
+      promise = offertaService.createResource(formData);
+    } else if (type === "edit") {
+      promise = offertaService.updateResource(id, formData);
+    } else if (type === "delete") {
+      promise = offertaService.deleteResource(id);
     }
 
+    if (promise) {
+      promise.then(() => {
+        NotificationProviderActions.openModal(
+          { icon: true, style: "success" },
+          "Operazione avvenuta con successo"
+        );
+        refreshTable();
+        closeModal();
+      });
+    }
+  };
 
-    return <>
-    <CountrySelector />
+  return (
     <GridTable
-    /*  inputSearchConfig={{
-       inputSearch: termValue,
-       handleInputSearch: handleInputSearch,
-       debouncedSearchTerm: termValue,
-     }} */
-     filter={filter}
-     setFilter={setFilter}
-     filterable={true}
-     initialPagination={pagination}
-     sortable={true}
-     setSorting={setSorting}
-     sorting={sorting}
-     getData={loadData}
-     columns={columns}
-     resizableWindow={true}
-     initialHeightWindow={800}
-     draggableWindow={true}
-     initialWidthWindow={900}
-     resizable={true}
-     actions={[
-       "show",
-       "edit",
-       "delete",
-       "create",
-     ]}
- 
-     formCrud={(row: any, type: string, closeModalCallback: any, refreshTable: any) => (
-       <>
+      filter={filter}
+      setFilter={setFilter}
+      filterable={true}
+      initialPagination={pagination}
+      sortable={true}
+      setSorting={setSorting}
+      sorting={sorting}
+      getData={loadData}
+      columns={columns}
+      resizableWindow={true}
+      initialHeightWindow={800}
+      draggableWindow={true}
+      initialWidthWindow={900}
+      resizable={true}
+      actions={(row) => {
+        const actions: string[] = ["create", "show"];
+        if (row?.outcome_type?.id === "P") {
+          actions.push("delete");
+        } else {
+          actions.push("edit", "delete");
+        }
+        return actions;
+      }}
+      formCrud={(
+        row: any,
+        type: string,
+        closeModalCallback: any,
+        refreshTable: any
+      ) => (
+        <>
           <OffertaCrud
-                 row={row}
-                 type={type}
-                 closeModalCallback={closeModalCallback}
-                 refreshTable={refreshTable}
-                 onSubmit={handleFormSubmit}
-               /> 
-       </>
-     )}
-   />
-</>
+            row={row}
+            type={type}
+            closeModalCallback={closeModalCallback}
+            refreshTable={refreshTable}
+            onSubmit={handleFormSubmit}
+          />
+        </>
+      )}
+    />
+  );
 }
