f5d91a7648942e34f469bf6a55a8bd83cc576eb6
mverde73@live.com <pasquale.guastamacchia@external.eni.com>
2024-09-03 11:08:27 +0200

    modified:   .env.production 	modified:   package.json 	modified:   webpack.config.js 	modified:   yarn.lock

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ const deps = require("./package.json").dependencies;
 const { FederatedTypesPlugin } = require("@module-federation/typescript");
 const webpack = require("webpack");
 
-
 const mfeConfig = {
   name: "common",
   filename: "remoteEntry.js",
@@ -22,11 +21,11 @@ const mfeConfig = {
     "./Window": "./src/components/Window/component",
     "./Button": "./src/components/Button/component",
     "./CustomListView": "./src/components/CustomListView/component",
-    "./AutoComplete":"./src/components/AutoComplete/component",
-    "./CountrySelector":"./src/components/CountrySelector/component",
-    "./AvatarIcon":"./src/components/AvatarIcon/component",
-    "./CustomCard":"./src/components/CustomCard/component",
-    
+    "./AutoComplete": "./src/components/AutoComplete/component",
+    "./CountrySelector": "./src/components/CountrySelector/component",
+    "./AvatarIcon": "./src/components/AvatarIcon/component",
+    "./CustomCard": "./src/components/CustomCard/component",
+
     "./services/AuthService": "./src/services/AuthService",
     "./services/BEService": "./src/services/BEService",
     "./services/BaseHTTPService": "./src/services/BaseHTTPService",
@@ -50,75 +49,71 @@ const mfeConfig = {
   },
 };
 
-
 module.exports = (_, argv) => {
-
-  require('dotenv').config({path:'./.env.'+argv.mode});
+  require("dotenv").config({ path: "./.env." + argv.mode });
 
   return {
-  output: {
-    publicPath: process.env.RELEASE_PATH,
-    clean:true
-  },
-  devtool:"source-map",
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
-  devServer: {
-    port: 3003,
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          {
-            loader: "file-loader",
+    output: {
+      publicPath: process.env.RELEASE_PATH,
+      clean: true,
+    },
+    devtool: "source-map",
+    resolve: {
+      extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    },
+    devServer: {
+      port: 3003,
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]",
+              },
+            },
+          ],
+        },
+        {
+          test: /\.m?js/,
+          type: "javascript/auto",
+          resolve: {
+            fullySpecified: false,
+          },
+        },
+        {
+          test: /\.(css|s[ac]ss)$/i,
+          use: ["style-loader", "css-loader", "postcss-loader"],
+        },
+        {
+          test: /\.json$/,
+          type: "json",
+        },
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
             options: {
-              name: "[name].[ext]",
+              presets: ["@babel/preset-react", "@babel/preset-env"],
             },
           },
-        ],
-      },
-      {
-        test: /\.m?js/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
         },
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.json$/,
-        type: "json",
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-react", "@babel/preset-env"],
-          },
-        },
-      },
+      ],
+    },
+    plugins: [
+      new ModuleFederationPlugin(mfeConfig),
+      //new FederatedTypesPlugin({ federationConfig: mfeConfig }),
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "index.html",
+        chunks: ["bundle"],
+      }),
+      new Dotenv({ path: "./.env." + argv.mode }),
     ],
-  },
-  plugins: [
-    new ModuleFederationPlugin(mfeConfig),
-    //new FederatedTypesPlugin({ federationConfig: mfeConfig }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
-      chunks: ["bundle"],
-    }),
-    new Dotenv({path:'./.env.'+argv.mode}),
-    ...(argv.mode==='production'?[new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1,
-    })]:[]),
-  ],
-}};
+  };
+};
