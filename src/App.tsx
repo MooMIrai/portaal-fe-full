811b8fa345ae2668360196a62eaf74913340b99a
CalogeroMiraglia95q <c.miraglia@xister.com>
2024-11-07 22:10:13 +0100

    fixed redirect

--- src/App.tsx
+++ src/App.tsx
@@ -19,72 +19,98 @@ import { Routes, useNavigate } from "react-router-dom";
 
 export const App = () => {
   const [routes, setRoutes] = useState<Array<any>>([]);
+  const [loaded, setLoaded] = useState<boolean>(false);
   const [token, setToken] = useState<string>();
   const navigate = useNavigate();
 
   useEffect(() => {
     try {
-      setToken(authService.getToken());
-    } catch { }
+      if (!loaded) {
+        setToken(authService.getToken());
+      }
+
+    }catch(e){
+      
+    }finally{
+      setLoaded(true);
+    }
   }, []);
 
   useEffect(() => {
     let r: Array<any> = [];
-    if (token) {
-      const lookupConfig = initLookupFn();
-      if (lookupConfig && lookupConfig.menuItems) {
-        r = [...r, ...lookupConfig.menuItems];
+    if (loaded) {
+      if (token) {
+        if(location.pathname == '/login'){
+          location.href = "/";
+        }else{
+        const lookupConfig = initLookupFn();
+        if (lookupConfig && lookupConfig.menuItems) {
+          r = [...r, ...lookupConfig.menuItems];
+        }
+        const salesConfig = initSalesFn();
+        if (salesConfig && salesConfig.menuItems) {
+          r = [...r, ...salesConfig.menuItems];
+        }
+
+        const hrConfig = initHrFn();
+        if (hrConfig && hrConfig.menuItems) {
+          r = [...r, ...hrConfig.menuItems];
+        }
+
+        const profileConfig = initProfileFn();
+        if (profileConfig && profileConfig.menuItems) {
+          r = [...r, ...profileConfig.menuItems];
+        }
+
+        const stockConfig = initStockFn();
+        if (stockConfig && stockConfig.menuItems) {
+          r = [...r, ...stockConfig.menuItems];
+        }
+
+        /* import("stock/Index").then(module => {
+          debugger;
+        }) */
+
+        window.addEventListener(
+          "LOGOUT",
+          () => {
+            setToken(undefined);
+          },
+          { once: true }
+        );
       }
-      const salesConfig = initSalesFn();
-      if (salesConfig && salesConfig.menuItems) {
-        r = [...r, ...salesConfig.menuItems];
+      } else if(location.pathname != '/login'){
+        
+        //navigate("/login", { replace: true });
+        location.href = "/login";
       }
 
-      const hrConfig = initHrFn();
-      if (hrConfig && hrConfig.menuItems) {
-        r = [...r, ...hrConfig.menuItems];
-      }
-
-      const profileConfig = initProfileFn();
-      if (profileConfig && profileConfig.menuItems) {
-        r = [...r, ...profileConfig.menuItems];
-      }
-
-      const stockConfig = initStockFn();
-      if (stockConfig && stockConfig.menuItems) {
-        r = [...r, ...stockConfig.menuItems];
-      }
-
-      window.addEventListener(
-        "LOGOUT",
-        () => {
-          setToken(undefined);
-        },
-        { once: true }
-      );
-    } else {
-      navigate("/login", { replace: true });
+      setRoutes(r);
     }
+  }, [token, loaded]);
 
-    setRoutes(r);
-  }, [token]);
 
-  if (!token && location.pathname != "/login") {
-    location.href = "/login";
-  }
-  if (!token) {
-    return <AuthRoutes />;
-  }
 
-  return (
-    <Theme>
-      <Drawer items={routes}>
-        <LookupsRoutes />
-        <SalesRoutes />
-        <HrRoutes />
-        <AuthVisibleRoutes />
-        <StockRoutes />
-      </Drawer>
-    </Theme>
-  );
+ if(!loaded){
+  return <div>Loading Auth data...</div>
+ }
+
+if (!token) {
+  return <AuthRoutes />;
+}
+
+
+return (
+  <Theme>
+    <Drawer items={routes}>
+      <LookupsRoutes />
+      <SalesRoutes />
+      <HrRoutes />
+      <AuthVisibleRoutes />
+      <StockRoutes />
+    </Drawer>
+  </Theme>
+);
+
+  
 };
