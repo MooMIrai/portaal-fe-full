ac6b293ea489c3429b9042668c103acdbe47fae0
Gianluca Scamperti <gscamperti@taal.it>
2024-12-11 17:55:26 +0100

    recruting

--- src/pages/Candidates/component.tsx
+++ src/pages/Candidates/component.tsx
@@ -2,10 +2,70 @@ import React, { useEffect, useState } from "react";
 import GridTable from "common/Table";
 import { candidatoService } from "../../services/candidatoService";
 import { CandidatiCrud } from "../../components/CandidatiCrud/component";
+import "bootstrap/dist/css/bootstrap.min.css"; // Importa il CSS di Bootstrap
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Importa i file JavaScript di Bootstrap (necessari per tooltip, modali, ecc.)
 
 export default function CandidatePage() {
 
-  function getMaxDateLog(assignment: any): Date | null {
+  const columns = [
+
+    { key: "Person.firstName", label: "Nome", type: "string", sortable: true, filter: "text" },
+    { key: "Person.lastName", label: "Cognome", type: "string", sortable: true, filter: "text" },
+    { key: "CandidateProfile.description", label: "Mansione", type: "string", sortable: true, filter: "text" },
+    {
+      key: "Person.PersonSkillAreas.SkillArea.name", label: "Skills", type: "custom", sortable: true, filter: "text", width: 250, render: (row) => {
+
+        if (row?.Person?.PersonSkillAreas == null || row.Person.PersonSkillAreas.lenght == 0)
+          return <td></td>;
+
+        let skills = row.Person.PersonSkillAreas;
+        let skillsDescription = skills.map(skill => skill.SkillArea.name).filter(name => name).join(", ");
+
+        if (skillsDescription.lenght > 50)
+        {
+
+        }
+
+        let substring = !skillsDescription || skillsDescription.lenght <= 50 ? skillsDescription : `${skillsDescription.substring(0, 46)} ...`;
+
+        return <td>
+                <span
+                  data-bs-toggle="tooltip"
+                  title={skillsDescription}
+                  className="d-inline-block text-truncate tooltip-bs"
+                  style={{ cursor: "pointer" }}
+                >
+                  {substring}
+                </span>
+              </td>;
+      }
+    },
+    {
+      key: "date_last_revision", label: "Data Ultima Revisione", type: "custom", sortable: true, filter: "date", width: 270, render: (row) => {
+
+        let assignment = getMaxDateLog(row.RecruitingAssignments);
+
+        if(assignment == null)
+          return <td></td>;
+        
+        let date_last_revision = new Date(assignment).toLocaleDateString();
+
+        return <td>{date_last_revision}</td>;
+      }
+    },
+  ];
+
+  const loadData = (
+    pagination: any,
+    filter: any,
+    sorting: any[],
+  ) => {
+
+    return candidatoService.search(pagination.currentPage, pagination.pageSize, filter, sorting, undefined, true)
+  }
+
+  const getMaxDateLog = (assignment: any): Date | null => {
+
     let maxDate: Date | null = null;
 
     const updateMaxDate = (date?: Date) => {
@@ -29,46 +89,7 @@ export default function CandidatePage() {
       updateMaxDate(item?.RecruitingFinalEvaluation?.date_log);
     });
 
-   
-
     return maxDate;
-  }
-
-
-  const columns = [
-
-    { key: "Person.firstName", label: "Nome", type: "string", sortable: true, filter: "text" },
-    { key: "Person.lastName", label: "Cognome", type: "string", sortable: true, filter: "text" },
-    { key: "CandidateProfile.description", label: "Mansione", type: "string", sortable: true, filter: "text" },
-    {
-      key: "skills", label: "Skills", type: "custom", sortable: true, filter: "text", render: (row) => {
-        if (row?.Person?.PersonSkillAreas == null || row.Person.PersonSkillAreas.lenght == 0)
-          return <td></td>;
-        let skills = row.Person.PersonSkillAreas;
-        let skillsDescription = skills.map(skill => skill.SkillArea.name).filter(name => name).join(", ");
-
-        return <td>{skillsDescription}</td>;
-      }
-    },
-    {
-      key: "date_last_revision", label: "Data Ultima Revisione", type: "custom", sortable: true, filter: "date", render: (row) => {
-        let assignment = getMaxDateLog(row.RecruitingAssignments);   
-        if(assignment == null)
-          return <td></td>;
-        let date_last_revision = new Date(assignment).toLocaleDateString();
-
-        return <td>{date_last_revision}</td>;
-      }
-    },
-  ];
-
-  const loadData = (
-    pagination: any,
-    filter: any,
-    sorting: any[],
-  ) => {
-
-    return candidatoService.search(pagination.currentPage, pagination.pageSize, filter, sorting, undefined, true)
   }
 
   return <GridTable
