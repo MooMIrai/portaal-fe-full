809e6845b545620cff6124ea63a31d8a88188a67
CalogeroMiraglia95q <c.miraglia@xister.com>
2025-01-24 15:52:40 +0100

    fixed style

--- @mf-types/common/Drawer.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './_types/components/Drawer/component';
-export { default } from './_types/components/Drawer/component';
--- @mf-types/common/_types/crossevents/MfeInitService.d.ts
+++
@@ -1,4 +0,0 @@
-import { DrawerItemType } from "../components/Drawer/component";
-export interface MfeInitProps {
-    routes: DrawerItemType[];
-}
