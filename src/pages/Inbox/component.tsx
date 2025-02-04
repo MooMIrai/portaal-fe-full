5f49c836c55c533d7b866edf8d4d601d90ca82e3
CalogeroMiraglia95q <c.miraglia@xister.com>
2025-02-04 13:48:17 +0100

    added notifications

--- src/pages/Inbox/component.tsx
+++ src/pages/Inbox/component.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 import styles from './style.module.scss';
 import Typography from 'common/Typography';
 import { notificationServiceHttp } from "../../services/notificationService";
+import SvgIcon from 'common/SvgIcon';
+import {starOutlineIcon, starIcon} from 'common/icons';
 
 export function InboxPage(){
 
@@ -20,8 +22,9 @@ export function InboxPage(){
 
         {
             notificationList?.map(n=><div key={n.id} className={styles.list+ ' ' +  (n.NotificationStatus.notificationStatus==='SENT'?styles.unread:'')}>
+                {n.isFlagged?<SvgIcon size="large" themeColor="warning" icon={starIcon} color={'yellow'}></SvgIcon>:<SvgIcon size="large" themeColor="warning" icon={starOutlineIcon}></SvgIcon>}
                 <Typography.p>{n.user_created}</Typography.p>
-                <Typography.p>{n.NotifyUser.content.title} - {n.NotifyUser.content.sub_title}</Typography.p>
+                <Typography.p>{n.NotifyUser.content.title} - <span>{n.NotifyUser.content.sub_title}</span></Typography.p>
                 <Typography.p>{new Date(n.NotifyUser.date_start).toLocaleDateString()}</Typography.p>
             </div>)
         }
