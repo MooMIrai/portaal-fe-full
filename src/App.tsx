26c93e94d34d186bdf48e4c439d142114cd6f9ca
CalogeroMiraglia95q <c.miraglia@xister.com>
2025-02-10 12:20:09 +0100

    added notification warning for permissions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from "react";
 import {  Route, Routes } from "react-router-dom";
-import { notificationService } from "./services/notification";
 import { InboxPage } from "./pages/Inbox/component";
 import { useSocketConnected } from "./hooks/useSocket";
 import { SentPage } from "./pages/Sent/component";
-
+import NotificationProviderActions from "common/providers/NotificationProvider";
 
 const App = () => {
 
@@ -15,7 +14,11 @@ const App = () => {
     if(Notification.permission!=='granted'){
       Notification.requestPermission().then((result) => {
           if(result==='denied'){
-            alert('Attenzione hai disabilitato le notifiche del browser, se vuoi essere al corrente dei messaggi in arrivo riattivale dalle impostazioni del browser.')
+            NotificationProviderActions.openModal(
+              { icon: true, style: "warning" },
+              "Attenzione hai disabilitato le notifiche del browser, se vuoi essere al corrente dei messaggi in arrivo riattivale dalle impostazioni del browser."
+          );
+            
           }
       });
     }
