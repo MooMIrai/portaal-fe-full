d7194f9e85e90da5f95644c4dbbe1789286c8d96
CalogeroMiraglia95q <c.miraglia@xister.com>
2025-02-10 18:26:10 +0100

    colore

--- src/components/AvatarIcon/component.tsx
+++ src/components/AvatarIcon/component.tsx
@@ -7,10 +7,31 @@ type AvatarIconProps = {
   initials: string;
 };
 
-export default function AvatarIcon(props: AvatarIconProps) {
+export default function AvatarIcon(props: AvatarIconProps & {name?:string}) {
+
+  const stringToColor=(name:string) =>{
+    let hash1 = 0, hash2 = 0;
+    
+    for (let i = 0; i < name.length; i++) {
+        if (i % 2 === 0) {
+            hash1 = name.charCodeAt(i) + ((hash1 << 5) - hash1);
+        } else {
+            hash2 = name.charCodeAt(i) + ((hash2 << 5) - hash2);
+        }
+    }
+
+    const h1 = Math.abs(hash1) % 360;
+    const h2 = Math.abs(hash2) % 360;
+    const s = 60 + (Math.abs(hash1 + hash2) % 30); // Saturazione tra 60% e 90%
+    const l = 50 + (Math.abs(hash1 - hash2) % 20); // Luminosità tra 50% e 70%
+
+    return `linear-gradient(135deg, hsl(${h1}, ${s}%, ${l}%) 0%, hsl(${h2}, ${s}%, ${l}%) 100%)`;
+}
+
+
   return (
     <BadgeContainer>
-      <Avatar type="initials">
+      <Avatar type="initials" style={props.name?{background:stringToColor(props.name)}:undefined}>
         <Label className="k-item-text">{props.initials}</Label>
       </Avatar>
     </BadgeContainer>
