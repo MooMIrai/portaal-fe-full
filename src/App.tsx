07e8d34b6c70850e5fe927de6d81c3854dae37e0
CalogeroMiraglia95q <c.miraglia@xister.com>
2025-02-20 20:07:04 +0100

    added role on navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 import RequestPage from './pages/Request/component';
 import CandidatePage from './pages/Candidates/component';
+import Routes from 'common/Routes';
 
 export default function(){
-    return <>
-    
-    <Routes>
-    <Route path={"/richieste"} element={<RequestPage />}></Route>
-    <Route path={"/richieste/:id/:candidateId"} element={<RequestPage />}></Route>
-    <Route path={"/candidati"} element={<CandidatePage />}></Route>
-    
+    return <Routes data={[
+      {
+        path:"/richieste",
+        element:<RequestPage />,
+        permissions:["hr_request_read"]
+      },
+      {
+        path:"/richieste/:id/:candidateId",
+        element:<RequestPage />,
+        permissions:["hr_request_read"]
+      },
+      {
+        path:"/candidati",
+        element:<CandidatePage />,
+        permissions:["hr_candidate_read"]
+      }
+    ]}>
+
+    {/* <Routes>
+      <Route element={<ProtectedRoute permissions={["test"]} />}>
+        <Route path={"/richieste"} element={<RequestPage />}></Route>
+      </Route>
+      <Route path={"/richieste/:id/:candidateId"} element={<RequestPage />}></Route>
+      <Route path={"/candidati"} element={<CandidatePage />}></Route>
+      
+    </Routes> */}
   </Routes>
-  </>
 }
