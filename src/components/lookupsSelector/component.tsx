7549c70c08b49933b736aff7e82b246af06610ff
CalogeroMiraglia95q <c.miraglia@xister.com>
2025-03-25 14:16:42 +0100

    removed role on lookups

--- src/components/lookupsSelector/component.tsx
+++ src/components/lookupsSelector/component.tsx
@@ -2,7 +2,7 @@ import withAutoComplete from "common/hoc/AutoComplete";
 
 const getLookups = () =>
   Promise.resolve([
-    { id: 1, name: "Role" },
+  
     { id: 2, name: "ProjectType" },
     { id: 3, name: "ActivityType" },
     { id: 4, name: "AccountStatus" },
