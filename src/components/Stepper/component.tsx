4d37d94ac5e08e1d40cacc6fffa34cdffdd0d0e2
Andrea Etna <aetna@taal.it>
2025-06-06 14:26:46 +0200

    edited stepper icons (green for valid steps, red for invalid steps)

--- src/components/Stepper/component.tsx
+++ src/components/Stepper/component.tsx
@@ -1,9 +1,11 @@
 import { Stepper } from "@progress/kendo-react-layout";
+import { checkCircleIcon, xCircleIcon } from "@progress/kendo-svg-icons";
 import React from "react";
+import "./style.css";
 
 export default function CustomStepper(props:any){
 
-
-    return <Stepper items={props.items} value={props.value} onChange={props.onChange} orientation={props.orientation}></Stepper>
+    return <Stepper errorSVGIcon={xCircleIcon} successSVGIcon={checkCircleIcon} items={props.items} value={props.value} 
+    onChange={props.onChange} orientation={props.orientation} className="recruitingStepper" style={{paddingTop: "10px"}}></Stepper>
 
 }
