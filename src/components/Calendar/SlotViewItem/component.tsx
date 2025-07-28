666b9892f44ff6933ed6b9840922b9f5f3a80f88
Andrea Etna <aetna@taal.it>
2025-07-28 10:14:48 +0200

    added custom background for overtime slots/days

--- src/components/Calendar/SlotViewItem/component.tsx
+++ src/components/Calendar/SlotViewItem/component.tsx
@@ -1,15 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
-import {
-  SchedulerViewSlot,
-  SchedulerViewSlotProps,
-} from "@progress/kendo-react-scheduler";
+import styles from "./styles.module.scss";
+import { SchedulerViewSlot, SchedulerViewSlotProps} from "@progress/kendo-react-scheduler";
 import { CalendarContext } from "../provider";
 import { useLongPress } from "@uidotdev/usehooks";
+
 interface ViewItemProps extends SchedulerViewSlotProps {
-  render?: (
-    slot: Record<string, any> | undefined,
-    closeModalCallback: () => void
-  ) => JSX.Element;
+  render?: (slot: Record<string, any> | undefined, closeModalCallback: () => void) => JSX.Element;
   disableDrag?: boolean;
   isFinalized?: boolean;
 }
@@ -18,11 +14,7 @@ const CustomViewSlot = (props: ViewItemProps) => {
 
   const { setStart, setEnd, setDrag, drag, selectedStart, selectedEnd, holidays, unavailableDays, date } = useContext(CalendarContext);
 
-
-  const attrs = useLongPress(() => {
-
-
-  },
+  const attrs = useLongPress(() => {},
     {
       onStart: (e) => {
         const ev: any = e
@@ -35,38 +27,37 @@ const CustomViewSlot = (props: ViewItemProps) => {
         }
       },
       threshold: 0
-    });
+    }
+  );
 
   const [active, setActive] = useState(false);
 
   useEffect(() => {
     if (drag && selectedStart && (props.start >= selectedStart) && (!selectedEnd || props.end <= selectedEnd)) {
       setActive(true);
+    } 
+    
+    else setActive(false);
 
-    } else {
-      setActive(false);
-
-    }
-  }, [drag, selectedEnd])
+  }, [drag, selectedEnd]);
 
 
   
   const notInMounth = props.start.getMonth() != date.getMonth() /* || props.isFinalized */;
   const notInContract = unavailableDays?.includes(props.start.getDate());
-
+  const overTime = props.items.map(item => item.dataItem).reduce((acc, curr) => acc + curr.hours, 0) > 8; //8 hours as full working day
   const isHoliday = holidays.some((h) => h === props.start.getDate()) && date.getMonth() === props.start.getMonth();
+  
   let bg = {};
-  if (isHoliday && !notInContract) {
-    bg = { background: 'rgba(255,0,0,0.2)' }
-  } else if (props.isFinalized) {
-    bg = { background: '#e9ecef' }
-  }
+  if (isHoliday && !notInContract) bg = { background: 'rgba(255,0,0,0.2)' }
+  else if (props.isFinalized) bg = { background: '#e9ecef' };
 
 
 
-  return <div style={{
+  return <div className={styles.slotContainer} style={{
     flex: 1,
     ...(active ? { background: '#f0f0f0' } : {}),
+    ...(overTime ? {background: "#E7F2FB"} : {}),
     ...((notInMounth || notInContract) ? { cursor: 'not-allowed' } : {}),
     ...(notInContract ? {background: "var(--kendo-color-base-subtle, #f5f5f6)"} : {})
   }}
